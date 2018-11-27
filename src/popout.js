38cabe9f7aea0994b5911e30748a23d2c551ccf6
rajdua22 <rajvirdu@usc.edu>
2018-11-27 04:34:35 -0800

    Add files via upload

---
+++ src/popout.js
@@ -0,0 +1,31 @@
+// This file is required by the index.html file and will
+// be executed in the renderer process for that window.
+// All of the Node.js APIs are available in this process.
+
+const ipc = require('electron').ipcRenderer
+
+let targetmatch
+
+ipc.on('crypto-info', function (event, arg) {
+    targetmatch = arg
+})
+
+ipc.on('crypto-data', function (event, arg) {
+    let amatch
+    for(const match in arg){
+        if(arg[match].slug === targetmatch){
+            amatch = arg[match]
+            break
+        }
+    }
+    const myName = amatch.name
+    const league = amatch.league.name
+    const status  = amatch.status
+    const img = amatch.league.image_url
+    document.getElementById('main').innerHTML = `
+    <img class="img" src="${img}">
+    <div class="name">Match: ${myName}</div>
+    <div class="league">League: ${league}</div>    
+    <div class ="league">Status: ${status}</div>
+    `
+})
